716812a38e2f7678544e03e34ea6946c13aab682
recklessMo <hpf311@gmail.com>
2017-10-12 17:48:20 +0800

    调整目录结构

---
+++ src/main/java/com/recklessMo/test/multiThread/future/CalThread.java
@@ -0,0 +1,10 @@
+package com.recklessMo.test.multiThread.future;
+
+/**
+ *
+ * 用于计算结果
+ *
+ * Created by hpf on 10/12/17.
+ */
+public class CalThread {
+}
---
+++ src/main/java/com/recklessMo/test/multiThread/future/MainThread.java
@@ -0,0 +1,10 @@
+package com.recklessMo.test.multiThread.future;
+
+/**
+ *
+ * 用于汇总结果
+ *
+ * Created by hpf on 10/12/17.
+ */
+public class MainThread {
+}
